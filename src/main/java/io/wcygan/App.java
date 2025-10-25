f86bb94146fdf5a5feb8f3e0711330ef75d1a748
Will Cygan <wcygan.io@gmail.com>
2025-10-24 19:06:46 -0500

    init project

---
+++ src/main/java/io/wcygan/App.java
@@ -0,0 +1,15 @@
+package io.wcygan;
+
+/**
+ * Minimal Hello World entry point.
+ */
+public final class App {
+
+    private App() {
+        // Static utility class
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
